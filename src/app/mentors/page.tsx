73078274f3ef064b4f233a5efdf96d4021498ad8
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-20 16:04:50 +0900

    delete: ログ出力処理の削除

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -4,6 +4,4 @@ export default async function Mentors() {
   // サポートメンバー一覧を取得
   const mentors = await getMentors();
 
-  // @TODO: 一時的にサポートメンバー一覧をログに出力
-  console.log(JSON.stringify(mentors, null, 2));
 }
