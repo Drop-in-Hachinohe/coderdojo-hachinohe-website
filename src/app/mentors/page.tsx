95612b410925d2362cfc834d6432bfd829a67372
yuta-sksn <sukekawa@thymefield.jp>
2024-09-20 21:47:06 +0900

    fix: position でグルーピングした mentors: MentorByPosition を表示する形に修正

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -3,10 +3,77 @@ import SectionTitle from '@/components/common/SectionTitle/SectionTitle';
 import getMentors from '@/features/mentors/api/getMentors';
 import MentorList from '@/components/common/MentorList/MentorList';
 import FadeInOnScrollContainer from '@/components/common/FadeInOnScrollContainer/FadeInOnScrollContainer';
+import {
+  PositionDefinitions,
+  sortingPositionsKeys,
+} from '@/features/mentors/types/keys';
+
+import type {
+  Position,
+  MentorByPosition,
+  Mentor,
+} from '@/features/mentors/types';
 
 export default async function Mentors() {
   // サポートメンバー一覧を取得
   const mentors = await getMentors();
+
+  // position でグルーピングした mentors 配列を生成する
+  const arrayOfMentorsByPosition: MentorByPosition[] = (() => {
+    // position を key とした (グルーピング) Object 配列を生成
+    const groupedByPosition = mentors.contents.reduce(
+      (acc, mentor) => {
+        if (!acc[mentor.position[0]]) {
+          acc[mentor.position[0]] = [];
+        }
+
+        acc[mentor.position[0]].push(mentor);
+        return acc;
+      },
+      {} as Record<Position, Mentor[]>,
+    );
+
+    // 更に sortingPositionsKeys の順序でソート mentors 配列を生成
+    const result = Object.entries(groupedByPosition)
+      // sortingPositionsKeys の順序で配列をソートする
+      .sort(
+        ([a], [b]) =>
+          sortingPositionsKeys.indexOf(a as Position) -
+          sortingPositionsKeys.indexOf(b as Position),
+      )
+      // MentorByPosition に変換
+      .map(([position, mentors]) => ({
+        position: position as Position,
+        mentors,
+      }));
+
+    return result;
+  })();
+
+  // MentorListSections JSX の定義
+  const MentorListSections = arrayOfMentorsByPosition.map(
+    (mentorsByPosition, index) => {
+      const mentorListTwClasses =
+        index === arrayOfMentorsByPosition.length - 1
+          ? 'my-5' // 最後の MentorList に当てる TW Class 群
+          : 'mb-12 mt-5 sm:mb-20 sm:mt-10'; // 最後以外の MentorList に当てる TW Class 群
+
+      return (
+        <FadeInOnScrollContainer key={index}>
+          <section>
+            <SectionTitle
+              title={PositionDefinitions[index].label}
+              className="mb-6"
+            />
+            <MentorList
+              mentors={mentorsByPosition.mentors}
+              className={mentorListTwClasses}
+            />
+          </section>
+        </FadeInOnScrollContainer>
+      );
+    },
+  );
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
@@ -17,25 +84,8 @@ export default async function Mentors() {
           <PageHeading label="サポートメンバー" className="mb-4" />
         </div>
 
-        {/* チャンピオン（主催者）一覧 */}
-        <FadeInOnScrollContainer>
-          <SectionTitle title="チャンピオン（主催者）" className="mb-6" />
-          <MentorList
-            mentors={mentors.contents}
-            position="champion"
-            className="mb-12 mt-5 sm:mb-20 sm:mt-10"
-          />
-        </FadeInOnScrollContainer>
-
-        {/* メンター一覧 */}
-        <FadeInOnScrollContainer>
-          <SectionTitle title="メンター" className="mb-6" />
-          <MentorList
-            mentors={mentors.contents}
-            position="mentor"
-            className="my-5"
-          />
-        </FadeInOnScrollContainer>
+        {/* サポートメンバー一覧 */}
+        {MentorListSections}
       </div>
     </main>
   );
