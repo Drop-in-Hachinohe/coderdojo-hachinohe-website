4968fcc221497f3b616162571f3a7b765b4c6902
yuta-sksn <sukekawa@thymefield.jp>
2024-09-22 03:47:15 +0900

    feat: 開催報告一覧ページの共通箇所を切り出した PaginatedReportList の実装

---
+++ src/components/pages/reports/PaginatedReportList/PaginatedReportList.tsx
@@ -0,0 +1,33 @@
+import FadeInOnScrollContainer from '@/components/common/FadeInOnScrollContainer/FadeInOnScrollContainer';
+import ReportList from '@/components/common/ReportList/ReportList';
+import Pagination from '@/components/common/Pagination/Pagination';
+
+import type { Report } from '@/features/reports/types';
+
+type PaginatedReportListProps = {
+  reports: Report[];
+  totalPageCount: number;
+  hrefPath: `/${string}/page/[page]${string}`;
+};
+
+export default function PaginatedReportList({
+  reports,
+  totalPageCount,
+  hrefPath,
+}: PaginatedReportListProps) {
+  return (
+    <>
+      {/* 開催報告一覧 */}
+      <FadeInOnScrollContainer>
+        <ReportList reports={reports} className="mt-5" />
+      </FadeInOnScrollContainer>
+
+      {/* ページネーション */}
+      <Pagination
+        hrefPath={hrefPath}
+        totalPageCount={totalPageCount}
+        className={totalPageCount > 1 ? 'mt-16' : '!hidden'}
+      />
+    </>
+  );
+}
