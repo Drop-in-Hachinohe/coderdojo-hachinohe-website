4cc8a727ec5d7b0741b78ac65afe26885c93c9e7
yuta-sksn <sukekawa@thymefield.jp>
2024-09-27 18:16:36 +0900

    fix: 関数名を適切なものに修正

--- src/features/report/api/getReports.ts
+++ src/features/report/api/getReports.ts
@@ -3,7 +3,7 @@ import { microCMS } from '@/libs/microCMSClient';
 import type { MicroCMSQueries } from 'microcms-js-sdk';
 import type { Report } from '../types';
 
-export default async function getNextEvents(queries?: MicroCMSQueries) {
+export default async function getReports(queries?: MicroCMSQueries) {
   return await microCMS.getList<Report>({
     endpoint: 'reports',
     customRequestInit: {
